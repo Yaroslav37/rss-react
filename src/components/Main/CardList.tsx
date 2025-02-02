47f3fc4ab250752e709c4b6bd1e24e81afdc60b2
Yaroslav <minenkovyaroslav@mail.ru>
2025-02-03 00:04:44 +0300

    feat: add empty CardList condition

--- src/components/Main/CardList.tsx
+++ src/components/Main/CardList.tsx
@@ -5,9 +5,15 @@ import { Card, CardListProps } from '../../types/types';
 
 export default class CardList extends Component<CardListProps> {
   render() {
+    const { results } = this.props;
+
+    if (results.length === 0) {
+      return <div>Nothing was found</div>;
+    }
+
     return (
       <div className="card-list">
-        {this.props.results.map((result: Card) => (
+        {results.map((result: Card) => (
           <CardItem key={result.name} {...result} />
         ))}
       </div>
